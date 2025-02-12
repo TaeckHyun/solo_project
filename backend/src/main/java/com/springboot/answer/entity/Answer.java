ff11627fc53fc7e6f734f6bc673cd0c17de8087f
택현 <tjsk2222@gmail.com>
2025-02-12 15:51:29 +0900

    Answer 엔티티 수정

--- backend/src/main/java/com/springboot/answer/entity/Answer.java
+++ backend/src/main/java/com/springboot/answer/entity/Answer.java
@@ -1,5 +1,6 @@
 package com.springboot.answer.entity;
 
+import com.springboot.member.entity.Member;
 import com.springboot.question.entity.Question;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,26 @@ public class Answer {
 
     // Answer와 Question은 1 : 1 관계
     @OneToOne
-    @JoinColumn(name = "QUESTION_ID")
+    @JoinColumn(name = "QUESTION_ID", nullable = false)
     private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID", nullable = false)
+    private Member member;
+
+    private AnswerStatus answerStatus = AnswerStatus.ANSWER_PUBLIC;
+
+    public enum AnswerStatus {
+        ANSWER_PUBLIC("공개 답변"),
+        ANSWER_SECRET("비밀 답변");
+
+        @Getter
+        private String status;
+
+        AnswerStatus(String status) {
+            this.status = status;
+        }
+    }
 
     // 동기화, 영속성 전이
     public void setQuestion(Question question) {
