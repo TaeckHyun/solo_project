a380805a4e93b8c6efcd905d60eb527c9102a734
택현 <tjsk2222@gmail.com>
2025-02-12 15:53:45 +0900

    member Service, Repository 수정

--- backend/src/main/java/com/springboot/member/service/MemberService.java
+++ backend/src/main/java/com/springboot/member/service/MemberService.java
@@ -1,12 +1,22 @@
 package com.springboot.member.service;
 
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
 import com.springboot.member.entity.Member;
+import com.springboot.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public Member createMember(Member member) {
         return null;
     }
@@ -25,5 +35,13 @@ public class MemberService {
 
     public void deleteMember() {
 
+    }
+
+    // 가입이 되어있는 회원인지를 먼저 검증도 해야함
+    public void verifyExistsEmail(String email) {
+        Optional<Member> findMember = memberRepository.findByEmail(email);
+        if (findMember.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+        }
     }
 }
