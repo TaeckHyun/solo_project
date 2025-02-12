d88108f2f4dbb66cafa56b1a5bcbeb0c06e45bda
택현 <tjsk2222@gmail.com>
2025-02-12 14:09:52 +0900

    Like 패키지 틀 구현

---
+++ backend/src/main/java/com/springboot/like/controller/LikeController.java
@@ -0,0 +1,9 @@
+package com.springboot.like.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/likes")
+public class LikeController {
+}
---
+++ backend/src/main/java/com/springboot/like/mapper/LikeMapper.java
@@ -0,0 +1,7 @@
+package com.springboot.like.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface LikeMapper {
+}
