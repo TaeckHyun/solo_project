25fa321249564876a7ea8d39209f331c9481a59d
택현 <tjsk2222@gmail.com>
2025-02-12 18:02:48 +0900

    Exception Code 상태 코드 추가

--- backend/src/main/java/com/springboot/exception/ExceptionCode.java
+++ backend/src/main/java/com/springboot/exception/ExceptionCode.java
@@ -6,7 +6,9 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
     QUESTION_EXISTS(409, "Question exists"),
-    QUESTION_NOT_FOUND(404, "Question not found");
+    QUESTION_NOT_FOUND(404, "Question not found"),
+    QUESTION_NOT_OWNER(403, "해당 질문의 작성자만 수정할 수 있습니다"),
+    QUESTION_ALREADY_ANSWERED(400, "답변 완료된 질문은 수정할 수 없습니다");
 //    COFFEE_NOT_FOUND(404, "Coffee not found"),
 //    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
 //    ORDER_NOT_FOUND(404, "Order not found"),
