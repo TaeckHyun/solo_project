d79b530585fbb5747c44f4dedc0ed1eab8568e08
택현 <tjsk2222@gmail.com>
2025-02-13 17:52:12 +0900

    ExceptionCode 상태 추가

--- backend/src/main/java/com/springboot/exception/ExceptionCode.java
+++ backend/src/main/java/com/springboot/exception/ExceptionCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
+    MEMBER_NOT_OWNER(403, "You are not the owner of this resource"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     QUESTION_NOT_OWNER(403, "해당 질문의 작성자만 수정할 수 있습니다"),
     QUESTION_ALREADY_ANSWERED(400, "답변 완료된 질문은 수정할 수 없습니다"),
