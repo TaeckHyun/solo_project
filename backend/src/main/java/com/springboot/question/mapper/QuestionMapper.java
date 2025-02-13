92a95eb6e7522503a57fb12245815000361d5f0f
택현 <tjsk2222@gmail.com>
2025-02-13 15:19:26 +0900

    Question Mapper 로직 추가

--- backend/src/main/java/com/springboot/question/mapper/QuestionMapper.java
+++ backend/src/main/java/com/springboot/question/mapper/QuestionMapper.java
@@ -1,7 +1,17 @@
 package com.springboot.question.mapper;
 
+import com.springboot.question.dto.QuestionPatchDto;
+import com.springboot.question.dto.QuestionPostDto;
+import com.springboot.question.dto.QuestionResponseDto;
+import com.springboot.question.entity.Question;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
+    Question questionPostDtoToQuestion(QuestionPostDto questionPostDto);
+    Question questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto);
+    QuestionResponseDto questionToQuestionResponseDto(Question question);
+    List<QuestionResponseDto> questionsToQuestionResponseDtos(List<Question> questions);
 }
