694344b917daf51e7d4c37531321f0e7c622aa30
택현 <tjsk2222@gmail.com>
2025-02-17 17:28:24 +0900

    파일 경로를 파라미터로 받기 위해 일부 수정

--- backend/src/main/java/com/springboot/config/StorageConfiguration.java
+++ backend/src/main/java/com/springboot/config/StorageConfiguration.java
@@ -2,13 +2,14 @@ package com.springboot.config;
 
 import com.springboot.question.service.FileSystemStorageService;
 import com.springboot.question.service.StorageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfiguration {
     @Bean
-    public StorageService fileSystemStorageService(){
-        return new FileSystemStorageService();
+    public StorageService fileSystemStorageService(@Value("${file.upload-dir}") String uploadDir){
+        return new FileSystemStorageService(uploadDir);
     }
 }
