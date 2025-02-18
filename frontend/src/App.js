c2ff3e0b361931e882282fe9d708cc97a2a985af
택현 <tjsk2222@gmail.com>
2025-02-18 18:38:26 +0900

    구현한 login, Register App.js로 호출출

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import { useState } from 'react';
-import Board from './Board';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
-  return (
-    <div>
-      <Board/>
-    </div>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={
+                    <ProtectedRoute>
+                        <h1>대시보드 (로그인 필요)</h1>
+                    </ProtectedRoute>
+                } />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+
