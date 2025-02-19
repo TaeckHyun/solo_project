9e65822da27b534e4d2278cdd4a1d1609f471c54
택현 <tjsk2222@gmail.com>
2025-02-19 20:56:08 +0900

    Login, Logout, Register 컴포넌트 JSX 추가가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/protectedRoute";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Register from "./page/Register";
+import Login from "./page/Login";
+import Logout from "./page/Logout";
 
 function App() {
     return (
         <Router>
+            <div className="flex justify-between items-center p-4 bg-gray-200">
+                <Link to="/login" className="mr-4">로그인</Link>
+                <Link to="/register" className="mr-4">회원가입</Link>
+                <Logout /> {/* 로그아웃 버튼 */}
+            </div>
             <Routes>
-                <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={
-                    <ProtectedRoute>
-                        <h1>대시보드 (로그인 필요)</h1>
-                    </ProtectedRoute>
-                } />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
